16bfd2403a409eae400250b82b546e9a6da8e2fa
Samuel Rodriguez <31121205+sammyrod@users.noreply.github.com>
2018-09-06 08:15:32 -0400

    Added Environments

--- python_mini_tutorial.py
+++ python_mini_tutorial.py
@@ -1,4 +1,56 @@
-# This is a quick tutorial about using python for data analysis! :)
+# Intro to Anaconda
+"""
+Documentation: https://conda.io/docs/user-guide/tasks/manage-environments.html
+
+Environment:
+    
+    Creating Environment
+    >create --name envname
+    
+    Activating Environment
+    >activate envname
+    
+    Deactivating Environment
+    >deactivate
+    
+    View List of Environments
+    >conda env list
+    
+    View Packages Installed in Environment - outside
+    >conda list -n envname
+    
+    View Packages Installed in Environment - inside
+    >conda list
+    
+    Removing Environment
+    >conda remove --name envname -all
+    
+Packages:
+
+    Installing Package
+    
+    Removing Package
+"""
+
+
+# Intro to Data Load
+
+# 1. Load CSV
+# 2. Load Flat File
+# 3. Determine Delimiters 
+
+
+
+# Intro to Numpy for Data Transformation
+
+
+
+
+# Intro to Pandas for Data Transformation
+
+
+
+
 
 # DATA QUALITY DIMENSIONS
 
@@ -8,8 +60,8 @@ It answers the following questions:
     1. Do the data include all non-blank values or unjustified blank(null) 
     values exist?
     2. Is the missing data optional?
-    3. Is all the requisite information available? 
-    4. Do any data values have missing elements?)
+    3. Is all the required information available? 
+    4. Do any data values have missing elements?
     5. Are they in an unusable state?
     6. Are all data sets and data items recorded?♠
 
@@ -26,7 +78,40 @@ It answers the following questions:
 Valuable Checks:
 
 """
-# 3. Timeliness
+
+# 3. Validity
+"""
+It answers the following questions:
+    1. Does the data match the rules?
+    2. Does the data match the agreed format?
+    3. Can the data be transformed to the required data type?
+
+    
+Valuable Checks:
+    
+"""
+# 4. Accuracy
+"""
+It answers the following questions:
+    1. Does the data reflect the data set?
+    2. Does the data describe the metadata?
+    
+Valuable Checks:
+    
+"""
+# 5. Consistency
+"""
+It answers the following questions:
+    1. Does the data agrees all accross a row?
+    2. Does the data reflect the metadata all accross the column?
+    3. Is the format the same per particular metadata?
+"""
+# 6. Integrity
+"""
+
+"""
+
+# 7. Timeliness
 """
 It answers the following questions:
     1. 
@@ -34,19 +119,11 @@ It answers the following questions:
 Valuable Checks:
 
 """
-# 4. Validity
-"""
-Does the data match the rules?
-"""
-# 5. Accuracy
-"""
-Does the data reflect the data set?
-"""
-# 6. Consistency
-"""
-Can we match the data set across data stores?
-"""
-# 7. Integrity
-"""
 
-"""
+
+
+
+
+
+
+
