f187313735bf7743c03c08c1445d57d2ce4bf69e
Samuel <sammyrod@live.com>
2022-03-24 23:22:38 -0400

    Added echo to see where is failing

--- django_apps/profileapi_app/deploy/setup.sh
+++ django_apps/profileapi_app/deploy/setup.sh
@@ -13,29 +13,35 @@ apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
 # Create project directory
+echo "Creating project directory..."
 mkdir -p $PROJECT_GIT_PATH
 git clone $PROJECT_GIT_URL $PROJECT_GIT_PATH
 
 # Create virtual environment
+echo "Creating virtual environment..."
 mkdir -p $PROJECT_BASE_PATH/env
 python3 -m venv $PROJECT_BASE_PATH/env
 
 # Install python packages
+echo "Installing python packages..."
 $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
 $PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
 
 # Run migrations and collectstatic
+echo "Migrating and static compiling..."
 cd $PROJECT_BASE_PATH
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 
 # Configure supervisor
+echo "Configuring supervisor..."
 cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
 # Configure nginx
+echo "Configuring nginx..."
 cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
