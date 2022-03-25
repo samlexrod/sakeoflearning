474865df29c5f6375d76dbb5a37f7e61a07c14fe
Samuel Rodriguez <31121205+samlexrod@users.noreply.github.com>
2022-03-24 23:33:57 -0400

    Update setup.sh
    Changes for server testing on remote repo

--- django_apps/profileapi_app/deploy/setup.sh
+++ django_apps/profileapi_app/deploy/setup.sh
@@ -8,14 +8,14 @@ PROJECT_GIT_URL='https://github.com/samlexrod/sakeoflearning.git'
 PROJECT_GIT_PATH='/usr/local/apps/profiles-rest-api'
 PROJECT_BASE_PATH= $PROJECT_GIT_PATH/sakeoflearning/django_apps/profileapi_app
 
-echo "Installing dependencies..."
-apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+# echo "Installing dependencies..."
+# apt-get update
+# apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
-# Create project directory
-echo "Creating project directory..."
-mkdir -p $PROJECT_GIT_PATH
-git clone $PROJECT_GIT_URL $PROJECT_GIT_PATH
+# # Create project directory
+# echo "Creating project directory..."
+# mkdir -p $PROJECT_GIT_PATH
+# git clone $PROJECT_GIT_URL $PROJECT_GIT_PATH
 
 # # Create virtual environment
 # echo "Creating virtual environment..."
