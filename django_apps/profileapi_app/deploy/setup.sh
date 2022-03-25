24601df7134ed7537b802edbec3e3a0a5674a614
Samuel <sammyrod@live.com>
2022-03-24 22:45:35 -0400

    Updated the git url. Ready to test deployment

--- django_apps/profileapi_app/deploy/setup.sh
+++ django_apps/profileapi_app/deploy/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/CHANGEME.git'
+PROJECT_GIT_URL='https://github.com/samlexrod/sakeoflearning.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
