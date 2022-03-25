61d1a1c9eb3be827f2efe4859bdb7c0aa88a20d6
Samuel <sammyrod@live.com>
2022-03-24 23:31:02 -0400

    commenting to test error on server

--- django_apps/profileapi_app/deploy/setup.sh
+++ django_apps/profileapi_app/deploy/setup.sh
@@ -17,34 +17,34 @@ echo "Creating project directory..."
 mkdir -p $PROJECT_GIT_PATH
 git clone $PROJECT_GIT_URL $PROJECT_GIT_PATH
 
-# Create virtual environment
-echo "Creating virtual environment..."
-mkdir -p $PROJECT_BASE_PATH/env
-python3 -m venv $PROJECT_BASE_PATH/env
+# # Create virtual environment
+# echo "Creating virtual environment..."
+# mkdir -p $PROJECT_BASE_PATH/env
+# python3 -m venv $PROJECT_BASE_PATH/env
 
-# Install python packages
-echo "Installing python packages..."
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
-$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
+# # Install python packages
+# echo "Installing python packages..."
+# $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+# $PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
 
-# Run migrations and collectstatic
-echo "Migrating and static compiling..."
-cd $PROJECT_BASE_PATH
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+# # Run migrations and collectstatic
+# echo "Migrating and static compiling..."
+# cd $PROJECT_BASE_PATH
+# $PROJECT_BASE_PATH/env/bin/python manage.py migrate
+# $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 
-# Configure supervisor
-echo "Configuring supervisor..."
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
-supervisorctl reread
-supervisorctl update
-supervisorctl restart profiles_api
+# # Configure supervisor
+# echo "Configuring supervisor..."
+# cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+# supervisorctl reread
+# supervisorctl update
+# supervisorctl restart profiles_api
 
-# Configure nginx
-echo "Configuring nginx..."
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
-systemctl restart nginx.service
+# # Configure nginx
+# echo "Configuring nginx..."
+# cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+# rm /etc/nginx/sites-enabled/default
+# ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+# systemctl restart nginx.service
 
 echo "DONE! :)"
