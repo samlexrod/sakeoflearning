f3b66dce4b4ce9b488f7892aa393fad487223231
Samuel <sammyrod@live.com>
2022-03-24 22:36:24 -0400

    Configured settings for deployment

--- django_apps/profileapi_app/profiles_project/settings.py
+++ django_apps/profileapi_app/profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^kc5y16-w7^8qkp#%-+4roqbridgb&n#t1ael3trx760cpy40$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
