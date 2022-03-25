316ca4366e4eafe2bb9c7c35fa7d8481a6c60e45
Samuel <sammyrod@live.com>
2022-03-25 00:03:52 -0400

    Updated settings and also the update.sh to reflect the new basepath on server

--- django_apps/profileapi_app/deploy/update.sh
+++ django_apps/profileapi_app/deploy/update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api/django_apps/profileapi_app'
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
--- django_apps/profileapi_app/profiles_project/settings.py
+++ django_apps/profileapi_app/profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '^kc5y16-w7^8qkp#%-+4roqbridgb&n#t1ael3trx760cpy40$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-204-236-214-130.compute-1.amazonaws.com', 
+    '127.0.0.1'
+    ]
 
 
 # Application definition
