406ace43e74dda971b5ce5deb4491945fb4168da
Samuel Rodriguez <31121205+samlexrod@users.noreply.github.com>
2022-04-05 15:54:48 -0400

    Create configserver.sh

---
+++ websocket/configserver.sh
@@ -0,0 +1,9 @@
+pip install websockets==9.1
+pip install papermill==2.3.3
+pip install ipython==7.0.1
+
+sudo ifconfig | grep -A1 eth2
+sudo lsof -i -P -n | grep LISTEN 
+
+cp serverwebsocket.py ~/SageMaker/serverwebsocket.py
+cp configserver.sh ~/SageMaker/configserver.sh
